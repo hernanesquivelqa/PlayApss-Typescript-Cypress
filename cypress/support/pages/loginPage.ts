e57737ffeb4e5f5b785e7a1aa5e4b8f178df427a
hernan <hernan97carp@hotmail.com>
2025-03-22 11:03:52 -0300

    add .env and modify main.yml

--- cypress/support/pages/loginPage.ts
+++ cypress/support/pages/loginPage.ts
@@ -1,7 +1,9 @@
 import { ILoginPage } from '../types/loginPage.interface';
-
+import * as dotenv from "dotenv";
+dotenv.config();
 export class LoginPage implements ILoginPage {
   url: string;
+  
   boxForm: () => Cypress.Chainable<JQuery<HTMLHtmlElement>>;
   usernameInput: () => Cypress.Chainable<JQuery<HTMLHtmlElement>>;
   passwordInput: () => Cypress.Chainable<JQuery<HTMLHtmlElement>>;
@@ -15,8 +17,16 @@ export class LoginPage implements ILoginPage {
   }
 
   fillForm(): void {
-    this.usernameInput().click().type('standard_user');
-    this.passwordInput().click().type('secret_sauce');
+    const username = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+    if (!username) {
+      throw new Error('USERNAME is not defined in Cypress environment variables');
+    }
+    if (!password) {
+      throw new Error('PASSWORD is not defined in Cypress environment variables');
+    }
+    this.usernameInput().click().type(username);
+    this.passwordInput().click().type(password);
     this.submitForm().click();
   }
 }
