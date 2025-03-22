30a972bf4a7ad2a013794d53a8afe288eb8acea6
hernan <hernan97carp@hotmail.com>
2025-03-22 11:08:23 -0300

    delete skip to the test TC2:Verify a successful login with valid credentials

--- cypress/e2e/sauce-demo/loginSauceDemo.spec.ts
+++ cypress/e2e/sauce-demo/loginSauceDemo.spec.ts
@@ -8,7 +8,7 @@ describe('Login Sauce Demo', () => {
   it('TC1: Verify the loading of the login form', () => {
     page.boxForm().should('be.visible');
   });
-  it.skip('TC2:Verify a successful login with valid credentials', () => {
+  it('TC2:Verify a successful login with valid credentials', () => {
     page.fillForm();
     cy.url().should('include', '/inventory.html');
   });
